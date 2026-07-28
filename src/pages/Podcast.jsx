309ffe55bdf4d488c8b16a0fed803d74f6476ddd
acholthitchanta <78499478+acholthitchanta@users.noreply.github.com>
2026-07-28 18:23:22 +0700

    podcast section styling

--- src/pages/Podcast.jsx
+++ src/pages/Podcast.jsx
@@ -3,17 +3,9 @@ import podcast2 from "../assets/podcast/podcast2.jpg";
 import { Placeholder } from "react-bootstrap";
 import { useState } from "react";
 
-function PodcastEmbed({title, src}) {
-    const [isLoaded, setIsLoaded] = useState(false);
-    
+function PodcastEmbed({title, src}) {    
     return (
-        <div>
-            {!isLoaded && (
-                <Placeholder as="div" animation="glow" className="episode-cover">
-                    <Placeholder xs={12} />
-                </Placeholder>
-            )}
-            
+        <div>      
             <iframe
                 data-testid="embed-iframe"
                 title={title}
@@ -70,47 +62,46 @@ export default function Podcast() {
             <img className="podcast-landing-logo" src={podcast2} alt="Solis and Luna Arts Podcast" />
         </section>
             
+        
         <section className="light-blue podcast-intro">
-            <h1>Welcome to the Solis and Luna Arts Podcast, hosted by Willow Yoo.</h1>
-            <p>The podcast, originally started in February 2024, centered on discussions with musicians and music therapists. Since then, we've reached 1.5k+ global impressions and expanded to interviews with 70+ interdisciplinary professionals, artists, and youth leaders about the usage of creativity and the arts for social good, innovation, and wellbeing.</p>
-            
-            <h4>Recognized by Spotify as...</h4>
-            <ul>
-                <li>A 2025 Rising Star - our growth outpaced 84% of other shows</li>
-                <li>A 2025 Fan Fave - our average rating was higher than 61% of other shows</li>
-                <li>A 2025 Most Shared Show - we received more shares than 91% of other shows</li>
-            </ul>
+            <section>
+                <h2>See our most recent episode:</h2>
+                
+                <div className="light-blue podcast-category">
+                    <PodcastEmbed
+                        title="Most Recent Episode"
+                        src="https://open.spotify.com/embed/show/4nyCM6CHrLnu43uTBFJ3VX?utm_source=generator&si=ca9288b6d6f54f79"
+                    />
+                </div>
+            </section>
+            <section>
+                <h1>Welcome to the Solis and Luna Arts Podcast, hosted by Willow Yoo.</h1>
+                <p>The podcast, originally started in February 2024, centered on discussions with musicians and music therapists. Since then, we've reached 1.5k+ global impressions and expanded to interviews with 70+ interdisciplinary professionals, artists, and youth leaders about the usage of creativity and the arts for social good, innovation, and wellbeing.</p>
+                
+                <h4>Recognized by Spotify as...</h4>
+                <ul>
+                    <li>A 2025 Rising Star - our growth outpaced 84% of other shows</li>
+                    <li>A 2025 Fan Fave - our average rating was higher than 61% of other shows</li>
+                    <li>A 2025 Most Shared Show - we received more shares than 91% of other shows</li>
+                </ul>
+            </section>
         </section>
         
-        <section>
+        <section className="dark-blue">
             <div className="dark-blue podcast-divider">
-                <h2>Some themes we've explored:</h2>
+                <h2>SOME THEMES WE'VE EXPLORED:</h2>
             </div>
 
             <div className="podcast-category-grid">
                 {playlistSections.map((playlist) => (
-                    <div className="light-blue podcast-category" key={playlist.id}>
-                        <h3>{playlist.title}</h3>
-                        
+                    <div className="podcast-category" key={playlist.id}>
                         <PodcastEmbed
                             title={playlist.embedTitle}
                             src={playlist.src}
                         />
+                        <h3>{playlist.title}</h3>
                     </div>
                 ))}
-            </div>
-        </section>
-        
-        <section>
-            <div className="dark-blue podcast-divider">
-                <h2>Check out our most recent episode:</h2>
-            </div>
-            
-            <div className="light-blue podcast-category">
-                <PodcastEmbed
-                    title="Most Recent Episode"
-                    src="https://open.spotify.com/embed/show/4nyCM6CHrLnu43uTBFJ3VX?utm_source=generator&si=ca9288b6d6f54f79"
-                />
             </div>
         </section>
     </main>
