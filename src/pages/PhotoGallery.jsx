de494b8e6ea67f2357c01dd2f180ea9b31e4eeda
acholthitchanta <78499478+acholthitchanta@users.noreply.github.com>
2026-07-30 13:04:57 +0700

    gallery

--- src/pages/PhotoGallery.jsx
+++ src/pages/PhotoGallery.jsx
@@ -6,12 +6,7 @@ export default function PhotoGallery() {
   return (
     <div>
       <Landing theme='light-blue' background='db' landingImg={landing} title={"PHOTO GALLERY"} description={"Solis and Luna Arts started in 2021 as a small school club in Bergen County, New Jersey by Chloey Cho."} />
-        <div className="section-medium dark-blue">
-        <h1>PHOTO GALLERY</h1>
-        <p>Take a look back at our performances, workshops, and the communities we've had the privilege to serve.</p>
-      </div>
-
-      <div className="section-medium light-blue">
+      <div className="section-medium dark-blue">
         <h1>COMING SOON</h1>
         <p>We're working on this page — check back soon!</p>
       </div>
